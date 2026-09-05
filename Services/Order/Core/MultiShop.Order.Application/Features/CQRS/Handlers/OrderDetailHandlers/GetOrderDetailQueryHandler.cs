c78685680876d6da5fdabb20f407581a101449b1
vasip <vasip.343@gmail.com>
2026-09-05 17:00:43 +0300

    OrderServiceAdded

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class GetOrderDetailQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<List<GetOrderDetailQueryResult>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(i => new GetOrderDetailQueryResult
+            {
+                Id = i.Id,
+                ProductId = i.ProductId,
+                ProductName = i.ProductName,
+                ProductPrice = i.ProductPrice,
+                ProductAmount = i.ProductAmount,
+                ProductTotalPrice = i.ProductTotalPrice,
+                OrderingId = i.OrderingId
+            } ).ToList();
+        }
+    }
+}
